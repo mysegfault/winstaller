0a87fd7c5cacf046b61d3ccde32b815cb850c1c1
mysegfault <max@wininup.com>
2016-07-22 18:59:40 +0200

    feat: detect Linux distribution feat: add dropbox source

--- modules/os.sh
+++ modules/os.sh
@@ -1,8 +1,26 @@
 
 OS=`uname`
+log "OS is $OS"
 
-if [ "$VERBOSE" = "1" ]; then
-	echo "OS is $OS"
+IS_LINUX=
+IS_DEBIAN=
+IS_UBUNTU=
+
+if [ `uname` == "Linux" ]; then
+    log "Is Linux!"
+    IS_LINUX=1
+fi
+
+if [ $IS_LINUX ]; then
+    if uname -a | grep -i "Debian" &> /dev/null; then
+        log "Is Debian!"
+        IS_DEBIAN=1
+    fi
+
+    if uname -a | grep -i "Ubuntu" &> /dev/null; then
+        log "Is Ubuntu!"
+        IS_UBUNTU=1
+    fi
 fi
 
 # Lowercase
@@ -12,7 +30,5 @@ case "${OS,}" in
             ;;
          
         *)
-            echo $"Your OS [$OS] is not supported yet."
-            exit 1
- 
+            die $"Your OS [$OS] is not supported yet." 1
 esac
